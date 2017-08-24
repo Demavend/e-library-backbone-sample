c8f5eba44836a9e87f7d992983781a1aaf671634
Demavend <demavend12345@gmail.com>
2017-08-24 15:59:49 +0300

    Create nav-bar

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
-let Book = Backbone.Model.extend({
+window.App = {
+    Models: {},
+    Views: {},
+    Collections: {},
+    Templates: {}
+};
+App.Templates.Book = `<div class='panel-heading'><h1 class='text-center'><%=title%>` +
+    `</h1></div><div class='panel-body'><div class='col-md-2'><img src='<%=img%>` +
+    `'width='100%'  class='img-responsive'></img></div><div class='col-md-8'><h4 class='text-justify'><%=description%>` +
+    `</h4></div><div class='col-md-2'><button class='btn btn-info openModal'` +
+    `>Show<br>summary</button></div></div>`;
+App.Templates.Search = `<input id="bookName" class="search-query" type="text" placeholder="Search">
+<a id="btnSearch" class="btn btn-info">Search</a>
+<a id="clean" class="btn btn-danger">Clean</a>`;
+App.Models.Search = Backbone.Model.extend({});
+App.Models.Book = Backbone.Model.extend({
     defaults: {
         title: 'Top secret (apparently).',
         author: 'Your name could be here.',
@@ -9,21 +24,22 @@ let Book = Backbone.Model.extend({
         img: 'i/Cover.gif'
     }
 });
-let template = `<div class='panel-heading'><h1 class='text-center'><%=title%>` +
-    `</h1></div><div class='panel-body'><div class='col-md-2'><img src='<%=img%>` +
-    `'width='100%'  class='img-responsive'></img></div><div class='col-md-8'><h4 class='text-justify'><%=description%>` +
-    `</h4></div><div class='col-md-2'><button class='btn btn-info openModal'` +
-    `>Show<br>summary</button></div></div>`;
-
-let BookCollection = Backbone.Collection.extend({
-    model: Book
+App.Collections.Library = Backbone.Collection.extend({
+    model: App.Models.Book
 });
-let Library = Backbone.Collection.extend({
-    model: Book
+App.Views.Search = Backbone.View.extend({
+    tagName: 'form',
+    id:'navbar-collapse',
+    className: 'navbar-form collapse navbar-collapse navbar-right',
+    template: _.template(App.Templates.Search),
+    render: function() {        
+        this.$el.html(this.template());
+        return this;
+    },
 });
-let ContainerView = Backbone.View.extend({
+App.Views.Container = Backbone.View.extend({
     initialize: function() {},
-    template: _.template(template),
+    template: _.template(App.Templates.Book),
     tagName: 'div',
     className: 'panel panel-success',
     initialize: function() {
@@ -34,29 +50,30 @@ let ContainerView = Backbone.View.extend({
         return this;
     },
 });
-let BookshelfView = Backbone.View.extend({
+App.Views.Bookshelf = Backbone.View.extend({
     tagName: 'div',
     className: 'panel-group',
     initialize: function() {},
     render: function() {
         this.collection.forEach((book) => {
-            let View = new ContainerView({
+            let view = new App.Views.Container({
                 model: book
             });
-            this.$el.append(View.render().el);
+            this.$el.append(view.render().el);
         }, this);
         return this;
     }
-
 });
+let search = new App.Views.Search;
+$('div.container-fluid').append(search.render().el);
 
 
 let library = [];
 for (let i = 0; i < 10; i++) {
-    let book = new Book;
+    let book = new App.Models.Book;
     library.push(book);
 }
-let bookshelf = new BookshelfView({
+let bookshelf = new App.Views.Bookshelf({
     collection: library
 });
-$('body').append(bookshelf.render().el);
+//$('body').append(bookshelf.render().el);
