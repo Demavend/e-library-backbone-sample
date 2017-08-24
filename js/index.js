851ca7472128d308c773b11959747ab6014c95e7
Demavend <demavend12345@gmail.com>
2017-08-24 17:29:26 +0300

    Create modal window

--- js/index.js
+++ js/index.js
@@ -9,10 +9,24 @@ App.Templates.Book = `<div class='panel-heading'><h1 class='text-center'><%=titl
     `'width='100%'  class='img-responsive'></img></div><div class='col-md-8'><h4 class='text-justify'><%=description%>` +
     `</h4></div><div class='col-md-2'><button class='btn btn-info openModal'` +
     `>Show<br>summary</button></div></div>`;
-App.Templates.Search = `<input id="bookName" class="search-query" type="text" placeholder="Search">
-<a id="btnSearch" class="btn btn-info">Search</a>
-<a id="clean" class="btn btn-danger">Clean</a>`;
+App.Templates.Search = `<input id='bookName' class='search-query' type='text' placeholder='Search'>
+<a id='btnSearch' class='btn btn-info'>Search</a>
+<a id='clean' class='btn btn-danger'>Clean</a>`;
+App.Templates.Modal = `<div class='modal-dialog'><div class='modal-content'>
+<div class='modal-header'><h4 class='text-center text-primary'><%=title%></h4>
+<button class='close' data-dismiss='modal'>x</button>
+</div><div class='modal-body'>
+<div id='bodyModal' class='coll'>
+Author:  <%=author%>;<br>
+Title:  <%=title%>;<br>
+Category:  <%=category%>;<br>
+Publisher:  <%=publisher%>;<br>
+Date:  <%=date%>;
+</div></div><div class='modal-footer'>
+<button class='btn btn-danger' data-dismiss='modal'>Закрыть</button></div>
+</div></div>`;
 App.Models.Search = Backbone.Model.extend({});
+App.Models.Modal = Backbone.Model.extend({});
 App.Models.Book = Backbone.Model.extend({
     defaults: {
         title: 'Top secret (apparently).',
@@ -27,15 +41,43 @@ App.Models.Book = Backbone.Model.extend({
 App.Collections.Library = Backbone.Collection.extend({
     model: App.Models.Book
 });
-App.Views.Search = Backbone.View.extend({
-    tagName: 'form',
-    id:'navbar-collapse',
-    className: 'navbar-form collapse navbar-collapse navbar-right',
-    template: _.template(App.Templates.Search),
-    render: function() {        
+App.Views.Modal = Backbone.View.extend({
+    tagName: 'div',
+    id: 'modal',
+    className: 'modal fade',
+    template: _.template(App.Templates.Modal),
+    render: function() {
         this.$el.html(this.template());
         return this;
     },
+});
+App.Views.Search = Backbone.View.extend({
+    tagName: 'form',
+    id: 'navbar-collapse',
+    className: 'navbar-form collapse navbar-collapse navbar-right',
+    template: _.template(App.Templates.Search),
+    render: function() {
+        this.$el.html(this.template());
+        return this;
+    },
+    events: {
+        'click #btnSearch': 'showBooks',
+        'click #clean': 'cleanAll'
+    },
+    showBooks: () => {
+        let library = [];
+        for (let i = 0; i < 10; i++) {
+            let book = new App.Models.Book;
+            library.push(book);
+        }
+        let bookshelf = new App.Views.Bookshelf({
+            collection: library
+        });
+        $('body').append(bookshelf.render().el);
+    },
+    cleanAll: () => {
+        $('div.panel-success').remove();
+    }
 });
 App.Views.Container = Backbone.View.extend({
     initialize: function() {},
@@ -62,18 +104,17 @@ App.Views.Bookshelf = Backbone.View.extend({
             this.$el.append(view.render().el);
         }, this);
         return this;
+    },
+    events: {
+        'click .openModal': 'showModal',
+    },
+    showModal: () => {
+        let modal = new App.Views.Modal({
+          model: book
+        });
+        $('#modal').append(modal.render().el);
+        $('#modal').modal();
     }
 });
 let search = new App.Views.Search;
 $('div.container-fluid').append(search.render().el);
-
-
-let library = [];
-for (let i = 0; i < 10; i++) {
-    let book = new App.Models.Book;
-    library.push(book);
-}
-let bookshelf = new App.Views.Bookshelf({
-    collection: library
-});
-//$('body').append(bookshelf.render().el);
